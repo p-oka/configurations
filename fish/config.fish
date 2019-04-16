5f7d58452c1a51392305ea62cf154af194802fec
Kohei Kataoka <kata0hka@gmail.com>
2019-04-17 02:43:29 +0900

    Fix style

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,17 @@
-# Bootstrap installation
+# ----------------- #
+#     BOOTSTRAP     #
+# ----------------- #
 
-## fisher
-## https://github.com/jorgebucaran/fisher
+## ref. https://github.com/jorgebucaran/fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
-# Key binding
+# ------------------- #
+#     KEY BINDING     #
+# ------------------- #
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
