88659f2eb5df8ea964c55e5bedec34f4289cb3ec
Kohei Kataoka <kata0hka@gmail.com>
2019-04-17 12:10:28 +0900

    Add fzf

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,7 @@ end
 # ------------------- # 
 
 function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
+  bind \cr peco_select_history
   bind \c] peco_select_ghq_repository
 end
 
